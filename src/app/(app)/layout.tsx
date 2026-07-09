32f69dc5eee93bb880546bb62ea837651b8c0b28
Futurewhitehat2024 <141867345+Futurewhitehat2024@users.noreply.github.com>
2026-07-08 22:35:33 -0700

    feat: initial Forge Web app (part 2)

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,1 +1,17 @@
-test
+import { ForgeProvider } from "@/components/forge-provider";
+import { Sidebar } from "@/components/layout/sidebar";
+import { Topbar } from "@/components/layout/topbar";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ForgeProvider>
+      <div className="min-h-screen bg-background">
+        <Sidebar />
+        <div className="pl-[240px]">
+          <Topbar />
+          <main className="p-6 lg:p-8">{children}</main>
+        </div>
+      </div>
+    </ForgeProvider>
+  );
+}
